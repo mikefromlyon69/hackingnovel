e1276ac4c834d50aeff2bfba14b956f5494a9af1
Mickaël Fosso <jazzkeats@gmail.com>
2021-05-18 22:31:03 +0200

    simplification chap1
    Decode directly on the page

--- src/index.js
+++ src/index.js
@@ -2,9 +2,8 @@ import { Hint} from './hints.js'
 
 const form = document.querySelector("#secretform");
 const rdv = document.querySelector("#rdv");
-const next = document.querySelector("#next");
 
-const hintText = "Base64";
+const hintText = "Lorsque l’on veut représenter des données binaires (une image, un exécutable) dans un document textuel, tel qu’un courriel, la transcription hexadécimale en ASCII des octets multiplierait la taille par deux, l'encodage en base64 permet de limiter cette augmentation. ";
 const hint_html = document.querySelector("#hint");
 const hint_details_html = document.querySelector("#hint-details");
 const hint = new Hint(hintText, hint_html, hint_details_html);
@@ -18,7 +17,6 @@ form.addEventListener('submit', function (event) {
 
 const checkRdv  = (rdv) => {
   if (rdv.value === "Mister Robot, ce soir!") {
-     next.innerHTML = "Prochaine PaGE";
      rdv.classList.remove("failed");
      rdv.classList.add("success");
      hint.successHint();
