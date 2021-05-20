08151c46954b6424ab3260f74acc4fc772710b19
Mickaël Fosso <jazzkeats@gmail.com>
2021-05-20 23:19:23 +0200

    base64 as a Class
    base64 class so juste one entry points in chapter 1

--- src/base64.js
+++ src/base64.js
@@ -1,32 +1,44 @@
-const  b64_to_utf8  = (str) => {
-  return window.atob(str); 
-};
+class Base64 {
+  constructor(b64_string) {
+   this.b64_string = b64_string;
+   this.decode_form = document.querySelector("#decode_form");
+   this.b64_input = document.querySelector("#b64str");
+   
+   this.decode_area = document.querySelector("#decoded");
+   this.set_listener();
+ }
 
-const decode_form = document.querySelector("#decode_form");
-const b64_input = document.querySelector("#b64str");
-const decode_area = document.querySelector("#decoded");
 
-decode_form.addEventListener('submit', function (event) {
+  b64_to_utf8  = (str) => {
+  	return window.atob(str); 
+  };
+
+  set_listener = () => {
+ 	const _this = this;
+    this.decode_form.addEventListener('submit', function (event) {
       event.preventDefault();
-      displaystring(b64_input.value);
- });
+      _this.displaystring(_this.b64_input.value);
+    });
+   }
 
-const checkMessage  = (message) => {
-  if (message === "Mister Robot, ce soir!") {
-     decode_area.classList.remove("failed");
-     decode_area.classList.add("success");
-  }
-  else
-  {
-     decode_area.classList.add("failed");
-     decode_area.classList.remove("success");
-  }
+	checkMessage  = (message) => {
+	    if (message === this.b64_string) {
+	     this.decode_area.classList.remove("failed");
+	     this.decode_area.classList.add("success");
+	  	}
+	  	else
+	  	{
+	     this.decode_area.classList.add("failed");
+	     this.decode_area.classList.remove("success");
+	  	}
+	}
+
+	displaystring = (str) => {
+	  const message_decoded = this.b64_to_utf8(str);
+	  this.decode_area.value = message_decoded;
+	  this.checkMessage(message_decoded);
+	};
 }
 
-const displaystring = (str) => {
-  const message_decoded = b64_to_utf8(str);
-  decode_area.value = message_decoded;
-  checkMessage(message_decoded);
-};
 
-
+export {Base64};
--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import { Hint} from './hints.js'
+import { Base64} from './base64.js'
 
 const hintText = "Lorsque l’on veut représenter des données binaires (une image, un exécutable) dans un document textuel, tel qu’un courriel, la transcription hexadécimale en ASCII des octets multiplierait la taille par deux, l'encodage en base64 permet de limiter cette augmentation. ";
 const hint = new Hint(hintText);
+
+const b64_string = "Mister Robot, ce soir!";
+const b64 = new Base64(b64_string);
 
 
 
