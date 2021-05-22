10733b8c5c5587aeee90f7b9fe00c2b7dbe15ccb
Mickaël Fosso <jazzkeats@gmail.com>
2021-05-22 20:59:44 +0200

    change validation
    validation chap2 is ls

--- src/chap2.js
+++ src/chap2.js
@@ -2,7 +2,7 @@ import { terminal } from './init_typed.js'
 import { Hint} from './init_hints.js'
 import { Validation} from './validation.js'
 
-const validation = new Validation("cat");
+const validation = new Validation("ls");
 
 
  				 
