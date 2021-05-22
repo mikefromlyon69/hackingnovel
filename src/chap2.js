b91d1eb14c9572782bd23012e9c3725be20f937e
Mickaël Fosso <jazzkeats@gmail.com>
2021-05-22 10:24:31 +0200

    removed chap2 uneccessary code
    code is now in hackterminal init

--- src/chap2.js
+++ src/chap2.js
@@ -11,14 +11,8 @@ const validation = new Validation("password1234");
 
 const terminit = " $ ";
 terminal.write(terminit);
-const files = "cutecat.txt secrets.txt"
 
-const cutecat_file = new File("cutecat.txt", "/\\_/\\ \r\n( o.o )\r\n > ^ <");
-const secrets_file = new File("secrets.txt", "jeffsmith@evilcorp.com password:password1234");
 
-const commands = ["ls", "cat"];
-const catArt = "/\\_/\\ \r\n( o.o )\r\n > ^ <";
-const secrets = "jeffsmith@evilcorp.com password:password1234"
  				 
 
 
