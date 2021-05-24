4470cc0771539f3a9cbb87cfb562aaa133115b6f
Mickaël Fosso <jazzkeats@gmail.com>
2021-05-24 21:58:53 +0200

    change mapper in sc

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { hint} from './init_hints.js'
 import { Base64} from './base64.js'
-
+const json = require('/mapper/mapper.json');
 
 const b64_string = "Mister Robot, ce soir!";
 const b64 = new Base64(b64_string);
